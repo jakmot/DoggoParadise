37d5f886e12abd6423c1e9d480f93d8a157fa295
jakub.motulewicz <jakubmotulewicz@gmail.com>
2021-05-23 19:12:14 +0200

    use FragmentContainerView

--- app/src/main/java/jakmot/com/doggoparadise/ContainerActivity.kt
+++ app/src/main/java/jakmot/com/doggoparadise/ContainerActivity.kt
@@ -9,10 +9,5 @@ class ContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.container_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, GalleryFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
